ba5aa39af58ad1c04a8ea2187dc3d2571463f25b
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-12 21:13:59 -0300

    card styl ok

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
+import "./index.css";
+import rogerinho from "./img/rogeirinho.jpg";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="btn"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">React, Programação Reativa e outros Paranauês</h1>
       </header>
+
+      <div className="piloto">
+        <img src={rogerinho} className="piloto-img-top" alt="..." />
+        <div className="piloto-body">
+          <h5 className="piloto-title">Rogerinho do Ingá</h5>
+          <p className="piloto-text">
+            Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis,
+            bibendum egestas augue arcu ut est. Interagi no mé,
+          </p>
+          <a href="/" className="btn btn-primary">
+            Página do Rogerinho
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
