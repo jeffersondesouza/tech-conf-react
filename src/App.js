d720599fa12475289d35e0b32ac27fadbe2f42c2
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-12 22:48:09 -0300

    list inti ok

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./index.css";
 
 import Header from "./components/Header";
 import Pilotos from "./components/Pilotos";
+import SucosConcentracao from "./components/SucosConcentracao";
 
 function App() {
   return (
     <div>
       <Header />
       <Pilotos />
+      <SucosConcentracao />
     </div>
   );
 }
